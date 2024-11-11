561ca0b64f811719fa6247c0d085376e556db3dc
Sabaoon <sabaoon.mohammadjamil@ontariotechu.net>
2024-11-11 18:05:30 -0500

    added more tutorials on home page

--- lib/screens/fitsync_home_page.dart
+++ lib/screens/fitsync_home_page.dart
@@ -91,10 +91,22 @@ class _FitsyncHomePageState extends State<FitsyncHomePage> {
                     title: "Crunch's Tutorial",
                     imageUrl: 'https://www.spotebi.com/wp-content/uploads/2014/10/crunches-exercise-illustration.jpg',
                   ),
-                  SlideWidget(title: 'Widget 2'),
-                  SlideWidget(title: 'Widget 3'),
-                  SlideWidget(title: 'Widget 4'),
-                  SlideWidget(title: 'Widget 5'),
+                  SlideWidgetWithImage(
+                      title: "Push-Ups Tutorial",
+                     imageUrl: 'https://tostpost.com/images/2018-Mar/28/da1c83d1c74739e363b38856d0a5b66b/1.jpg',
+                  ),
+                SlideWidgetWithImage(
+                  title: "Sit-Ups Tutorial",
+                  imageUrl: 'https://www.cdn.spotebi.com/wp-content/uploads/2014/10/squat-exercise-illustration.jpg',
+                ),
+                  SlideWidgetWithImage(
+                      title: 'Bicep Curls Tutorial',
+                      imageUrl: 'https://i.pinimg.com/originals/8f/40/fd/8f40fdace543223c4043dfd1adf36cf6.png',
+                  ),
+                  SlideWidgetWithImage(
+                      title: 'Leg Curls Tutorials',
+                      imageUrl: 'https://th.bing.com/th/id/R.5f2642f47b331e04d7b144b6813325c1?rik=OqYVd7pXpK4DiA&riu=http%3a%2f%2fworkoutlabs.com%2fwp-content%2fuploads%2fwatermarked%2fSeated_Leg_curl.png&ehk=8M7eWzedvaSSyv8JYhrI98Ld4WwTjo9hiw7Up8a4Ei4%3d&risl=&pid=ImgRaw&r=0',
+                  ),
                 ],
               ),
             ),
